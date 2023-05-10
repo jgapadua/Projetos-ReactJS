d95e0f3e7ad01ff139ae609e775c8aaa0332ec24
jgapadua <joaogabrielpadua@gmail.com>
2023-05-10 15:01:17 -0300

    listando itens

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,10 +1,23 @@
+import {useState} from 'react';
 import * as C from './App.styles';
+import {Item} from './types/Item'
+import { ListItem } from './components/ListItem';
+
 
  const App=()=> {
+  const [list, setList]=useState<Item[]>([
+    {id:1, name:'Comprar o pão na padaria', done:false},
+    {id:2, name:'Comprar um bolo na padaria', done:false},
+  ]);
+
   return (
       <C.Container>
       <C.Area>
       <C.Header>Lista de Tarefas</C.Header>
+        
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item}/>
+        ))}
       </C.Area>
       </C.Container>
   );
---
+++ todolist/src/components/ListItem/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container=styled.div`
+  background-color: #17181f;
+  color: red;
+  min-height: 100vh;
+`;
