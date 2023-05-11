dcda55b9da434a11f9208e1980a72e2ab1ace001
jgapadua <joaogabrielpadua@gmail.com>
2023-05-11 12:33:14 -0300

    checkbox

--- todolist/src/components/ListItem/styles.ts
+++ todolist/src/components/ListItem/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container=styled.div`
-  background-color: #17181f;
+  /* background-color: 17181f;
   color: red;
-  min-height: 100vh;
+  min-height: 100vh; */
 `;
