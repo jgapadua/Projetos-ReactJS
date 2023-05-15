bd0617735b145f78a463d2fe6c3bb173c86c85a9
jgapadua <joaogabrielpadua@gmail.com>
2023-05-15 10:38:33 -0300

    first commit

---
+++ galeria-fotos/src/libs/fireabase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCR91XQXj7OB8dXimD9kZbEweuBR4g3PkI",
+  authDomain: "galeriafotos-80b7c.firebaseapp.com",
+  projectId: "galeriafotos-80b7c",
+  storageBucket: "galeriafotos-80b7c.appspot.com",
+  messagingSenderId: "662830649164",
+  appId: "1:662830649164:web:9d65442049d1231da289e9"
+};
