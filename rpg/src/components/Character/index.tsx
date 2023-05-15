74ae092d92e62a2eb2cc8f8765712786b219985a
jgapadua <joaogabrielpadua@gmail.com>
2023-05-15 11:28:32 -0300

    posicionando character

---
+++ rpg/src/components/Character/index.tsx
@@ -0,0 +1,13 @@
+import * as C from './styles';
+
+type Props={
+    x: number;
+    y: number;
+}
+
+export const Character = ({x,y}:Props)=>{
+  return(
+    <C.Container>
+    </C.Container>
+  )
+}
