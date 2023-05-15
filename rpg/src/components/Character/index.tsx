d8808e4a8e989034ea367ca15538402cff0d9a04
jgapadua <joaogabrielpadua@gmail.com>
2023-05-15 12:06:07 -0300

    comando de movimento

--- rpg/src/components/Character/index.tsx
+++ rpg/src/components/Character/index.tsx
@@ -5,9 +5,15 @@ type Props={
     y: number;
 }
 
-export const Character = ({x,y}:Props)=>{
+export const Character = ({x, y}: Props)=>{
+    const size=30;
+
   return(
-    <C.Container>
+    <C.Container
+      size={size}
+      left={x*size}
+      top={y*size}
+    >
     </C.Container>
   )
 }
