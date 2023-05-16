e5d38b88a0c5359881b6f765b0959341776573db
jgapadua <joaogabrielpadua@gmail.com>
2023-05-16 11:19:59 -0300

    estilizei theme

--- multiform/src/App.tsx
+++ multiform/src/App.tsx
@@ -1,11 +1,17 @@
+import React from 'react';
 import { Router } from './router';
 import {FormProvider} from './contexts/FormContext';
 
+import { GlobalStyles } from './styles/GlobalStyles';
+
 const App = () => {
   return (
+    <React.Fragment>
+    <GlobalStyles />
     <FormProvider>
       <Router />
-    </FormProvider>
+      </FormProvider>
+      </React.Fragment>
   );
 }
 
