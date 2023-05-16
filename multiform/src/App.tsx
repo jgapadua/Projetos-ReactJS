f900aa7bdde811b1b47fa4e8766e9cbc3418c638
jgapadua <joaogabrielpadua@gmail.com>
2023-05-16 09:29:08 -0300

    arrumei o router

--- multiform/src/App.tsx
+++ multiform/src/App.tsx
@@ -1,19 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Router } from './router';
 
-import { FormStep1 } from './pages/FormStep1';
-import { FormStep2 } from './pages/FormStep2';
-import { FormStep3 } from './pages/FormStep3';
-
- const App=()=>{
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<FormStep1 />} />
-        <Route path='/step2' element={<FormStep2 />} />
-        <Route path='/step3' element={<FormStep3 />} />
-        </Routes>
-    </BrowserRouter>
+      <Router />
   );
 }
-export default App;
 
+export default App;
