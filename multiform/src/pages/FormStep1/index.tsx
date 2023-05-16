201da39a3b298b932aec00e9a53ebb65ebca433b
jgapadua <joaogabrielpadua@gmail.com>
2023-05-16 12:25:54 -0300

    context page 1

--- multiform/src/pages/FormStep1/index.tsx
+++ multiform/src/pages/FormStep1/index.tsx
@@ -1,18 +1,35 @@
 import {useNavigate} from 'react-router-dom'
 import * as C from './styles';
+import {useForm, FormActions} from '../../contexts/FormContext'
 import {Theme} from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1=()=>{
   const navigate = useNavigate();
-  
+  const {state, dispatch} = useForm();
+
+  useEffect(() => {
+    dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 1
+    });
+  }, []);
+
   const handleNextStep = ()=>{
     navigate('/step2');
+  }
+
+  const handleNameChange = (e:ChangeEvent<HTMLInputElement>)=>{
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value
+    });
   }
 
   return(
     <Theme>
       <C.Container>
-        <p>Passo 1/3</p>
+        <p>Passo {state.currentStep}/3</p>
         <h1>Vamos começar com seu nome</h1>
         <p>Preencha o campo abaixo com seu nome completo.</p>
         <hr/>
@@ -21,6 +38,8 @@ export const FormStep1=()=>{
             <input
                 type="text"
                 autoFocus
+                value={state.name}
+                onChange={handleNameChange}
             />
         </label>
         <button onClick={handleNextStep}>Próximo</button>
