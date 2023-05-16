dca8da94b1eccd605f27560a3845826a679a783f
jgapadua <joaogabrielpadua@gmail.com>
2023-05-16 11:48:07 -0300

    pagina passo 1

--- multiform/src/pages/FormStep1/index.tsx
+++ multiform/src/pages/FormStep1/index.tsx
@@ -5,7 +5,18 @@ export const FormStep1=()=>{
   return(
     <Theme>
       <C.Container>
-        Passo 1
+        <p>Passo 1/3</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+        <hr/>
+        <label>
+            Seu nome completo
+            <input
+                type="text"
+                autoFocus
+            />
+        </label>
+        <button onClick={handleNextStep}>Próximo</button>
       </C.Container>
     </Theme>
   );
